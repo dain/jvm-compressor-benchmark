3a411793434811c3d7b8a2f19c2afb85309e1658
Dain Sundstrom <dain@iq80.com>
2011-09-26 01:39:09 -0700

    Added test configuration for just snappy tests

---
+++ bin/run-snappy.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+ 
+echo "About to run minimal sanity test on 3 input files, couple of codecs"
+
+java -server -cp lib/japex/\* \
+-server \
+-XX:+UseCompressedOops \
+-Xms400M \
+-Xmx400M \
+-Djava.awt.headless=true \
+-Djapex.runsPerDriver=1 \
+-Djapex.warmupTime=30 \
+-Djapex.runTime=60 \
+-Djapex.numberOfThreads=1 \
+-Djapex.reportsDirectory=reports/snappy \
+-Djapex.plotGroupSize=5 \
+-Djapex.inputDir="testdata/canterbury" com.sun.japex.Japex \
+ cfg/tests-snappy.xml
+
+echo "Done!";
